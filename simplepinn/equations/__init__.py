07f65f91fd1a7427605660329ef46cc34827e618
mohammadxe <mohammad.m.asghari1@gmail.com>
2026-05-05 09:26:09 +0200

    Add LaplaceEquation implementation

--- simplepinn/equations/__init__.py
+++ simplepinn/equations/__init__.py
@@ -2,3 +2,4 @@ from .heat import HeatEquation
 from .burgers import BurgersEquation
 from .wave import WaveEquation
 from .poisson import PoissonEquation
+from .laplace import LaplaceEquation
--- simplepinn/equations/laplace.py
+++ simplepinn/equations/laplace.py
@@ -1,1 +1,19 @@
+from simplepinn.core.grad import grad
+from simplepinn.equations.base import BaseEquation
 
+
+class LaplaceEquation(BaseEquation):
+    """
+    1D Laplace equation:
+        u_xx = 0
+
+    Residual form:
+        u_xx = 0
+    """
+
+    def residual(self, model, coords):
+        x = coords["x"]
+        inputs = x
+        u = model(inputs)
+        u_xx = grad(u, x, order=2)
+        return u_xx
