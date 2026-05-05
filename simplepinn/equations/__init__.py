eeed1f893d5c8df60be0d445a6c0d38312ce376d
mohammadxe <mohammad.m.asghari1@gmail.com>
2026-05-05 09:19:19 +0200

    Add PoissonEquation implementation

--- simplepinn/equations/__init__.py
+++ simplepinn/equations/__init__.py
@@ -1,3 +1,4 @@
 from .heat import HeatEquation
 from .burgers import BurgersEquation
 from .wave import WaveEquation
+from .poisson import PoissonEquation
--- simplepinn/equations/poisson.py
+++ simplepinn/equations/poisson.py
@@ -1,1 +1,23 @@
+from simplepinn.core.grad import grad
+from simplepinn.equations.base import BaseEquation
 
+
+class PoissonEquation(BaseEquation):
+    """
+    1D Poisson equation:
+        u_xx = f(x)
+
+    Residual form:
+        u_xx - f(x) = 0
+    """
+
+    def __init__(self, source):
+        self.source = source
+
+    def residual(self, model, coords):
+        x = coords["x"]
+        inputs = x
+        u = model(inputs)
+        u_xx = grad(u, x, order=2)
+        f = self.source(x)
+        return u_xx - f
