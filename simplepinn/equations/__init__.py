f121c37c54b9bb906a58060d46eca40c57de08bc
mohammadxe <mohammad.m.asghari1@gmail.com>
2026-05-05 09:28:37 +0200

    Add AdvectionEquation implementation

--- simplepinn/equations/__init__.py
+++ simplepinn/equations/__init__.py
@@ -3,3 +3,4 @@ from .burgers import BurgersEquation
 from .wave import WaveEquation
 from .poisson import PoissonEquation
 from .laplace import LaplaceEquation
+from .advection import AdvectionEquation
--- simplepinn/equations/advection.py
+++ simplepinn/equations/advection.py
@@ -1,1 +1,28 @@
+import torch
+from simplepinn.core.grad import grad
+from simplepinn.equations.base import BaseEquation
 
+
+class AdvectionEquation(BaseEquation):
+    """
+    1D advection equation:
+        u_t + c * u_x = 0
+
+    Residual form:
+        u_t + c * u_x = 0
+    """
+
+    def __init__(self, c=1.0):
+        self.c = c
+
+    def residual(self, model, coords):
+        x = coords["x"]
+        t = coords["t"]
+
+        inputs = torch.cat([x, t], dim=1)
+        u = model(inputs)
+
+        u_t = grad(u, t)
+        u_x = grad(u, x)
+
+        return u_t + self.c * u_x
